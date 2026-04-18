e924b3e53e3512c2593edafaf6b7399ce0d04942
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-18 22:47:41 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-             
+            //var products = ListGenerator.ProductList;
+            //var customers = ListGenerator.CustomerList;
+            #region Q1
+            //var q1 = products.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
         }
     }
 }
