dace81ba9724ec00879e253968d79a69cab4ebf5
mikhaeilryad35-web <mikhaeilryad35@gmail.com>
2026-04-18 22:53:55 +0200

    Q9

--- Program.cs
+++ Program.cs
@@ -53,6 +53,18 @@
             //                  .Select(g => g.Key);
             #endregion
 
+            #region Q9
+            //var q9 = from c in customers
+            //         group c by c.Country into g
+            //         select new
+            //         {
+            //             Country = g.Key,
+            //             Count = g.Count(),
+            //             TotalOrderValue = g.Sum(c => c.Orders.Sum(o => o.Total))
+            //         };
+
+            #endregion
+
         }
     }
 }
